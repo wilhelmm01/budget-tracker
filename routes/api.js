746f100db209f7130e0460b6199f2473d61996ba
Your Name <matt14w@gmail.com>
2022-03-20 14:33:16 -0400

    updated get

--- routes/api.js
+++ routes/api.js
@@ -21,14 +21,14 @@ router.post("/api/transaction/bulk", ({body}, res) => {
     });
 });
 
-router.get("/api/transaction", (req, res) => {
-  Transaction.find({}).sort({date: -1})
-    .then(dbTransaction => {
-      res.json(dbTransaction);
-    })
-    .catch(err => {
-      res.status(404).json(err);
-    });
-});
+// router.get("/api/transaction", (req, res) => {
+//   Transaction.find({}).sort({date: -1})
+//     .then(dbTransaction => {
+//       res.json(dbTransaction);
+//     })
+//     .catch(err => {
+//       res.status(404).json(err);
+//     });
+// });
 
 module.exports = router;
