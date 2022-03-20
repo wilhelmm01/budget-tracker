d534a80ba93723c95cf7700f404bd29d91aaa4b0
Your Name <matt14w@gmail.com>
2022-03-20 14:46:03 -0400

    updated README

--- public/service-worker.js
+++ public/service-worker.js
@@ -49,10 +49,10 @@ self.addEventListener('fetch', function (e) {
     console.log('fetch request : ' + e.request.url);
     e.respondWith(
         caches.match(e.request).then(function (request) {
-            if (request) { // if cache is available, respond with cache
+            if (request) { 
                 console.log('responding with cache : ' + e.request.url);
                 return request
-            } else {       // if there are no cache, try fetching request
+            } else {       
                 console.log('file is not cached, fetching : ' + e.request.url);
                 return fetch(e.request)
             }
--- server.js
+++ server.js
@@ -16,10 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
+
 
 mongoose.connect(
     MONGODB_URI,
